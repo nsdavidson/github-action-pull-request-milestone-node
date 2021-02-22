3863beb26411146fdf23b45f991140ece8ebc518
Nolan Davidson <nolan.davidson@gmail.com>
2021-02-21 20:49:31 -0500

    guard rails

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("This is my action, and it's working!");
+const { stripIndent } = require("common-tags")
+
+async function action() {
+    const { stripIndent } = require("common-tags");
+
+    const core = require("@actions/core");
+    const github = require("@actions/github");
+
+    const event = process.env.GITHUB_EVENT_NAME;
+    const payload = require(process.env.GITHUB_EVENT_PATH);
+
+    if (event != "pull_request" || payload.action != "closed") {
+        core.setFailed(stripIndent`
+            This action only runs on pull_request.closed
+            Found: ${event}.${payload.action}
+        `);
+        return;
+    }
+
+    if (!payload.pull_request.merged) {
+        core.warning("Pull request closed without merge");
+        return;
+    }
+}
+
+if (require.main === module) {
+    action();
+}
+
+module.exports = action;
